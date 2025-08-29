51b5ad17fd3917a5c1dae27bcfa28f229f7e1d57
Jamie <f20040901@gmail.com>
2025-08-29 14:39:07 +0800

    修复uniapp前端配置错误

--- frontend/mobile/uniapp/App.vue
+++ frontend/mobile/uniapp/App.vue
@@ -1,23 +1,17 @@
-<script setup>
-	import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
-	
-	onLaunch(() => {
-		console.log('App Launch')
-	})
-	
-	onShow(() => {
-		console.log('App Show')
-	})
-	
-	onHide(() => {
-		console.log('App Hide')
-	})
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
 </script>
 
 <style>
 	/*每个页面公共css */
-	page {
-		background-color: #f8f8f8;
-		height: 100%;
-	}
 </style>
